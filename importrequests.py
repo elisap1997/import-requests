14d4371515f1d7c7526ee6361a188197df946d7e
Elisa Pavesi <elisa.r.pavesi@gmail.com>
2024-07-12 17:08:01 -0700

    changed link

--- importrequests.py
+++ importrequests.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Make a request to https://scrapepark.org
+# Make a request
 page = requests.get(
-    "https://scrapepark.org")
+    "https://scrapepark.org/#products")
 soup = BeautifulSoup(page.content, 'html.parser')
 
 # Extract title of page
-page_title = soup.title.text
+page_title = soup.title
+
+# Extract body of page
+page_body = soup.body
+
+# Extract head of page
+page_head = soup.head
 
 # print the result
-print(page_title)
+print(page_title, page_head)
