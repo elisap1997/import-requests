34898485cdeaa8c36071bb661641648bd4130489
Elisa Pavesi <elisa.r.pavesi@gmail.com>
2024-07-18 17:21:04 -0700

    installing csv capability

--- importrequests.py
+++ importrequests.py
@@ -1,19 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
-
+import csv
 # Make a request
 page = requests.get(
     "https://scrapepark.org/#products")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Extract title of page
-page_title = soup.title
+# Create top_items as empty list
+all_products = []
 
-# Extract body of page
-page_body = soup.body
+# Extract and store in top_items according to instructions on the left
+products = soup.select('div.thumbnail')
+for product in products:
+    name = product.select('h4 > a')[0].text.strip()
+    description = product.select('p.description')[0].text.strip()
+    price = product.select('h4.price')[0].text.strip()
+    reviews = product.select('div.ratings')[0].text.strip()
+    image = product.select('img')[0].get('src')
 
-# Extract head of page
-page_head = soup.head
+    all_products.append({
+        "name": name,
+        "description": description,
+        "price": price,
+        "reviews": reviews,
+        "image": image
+    })
 
-# print the result
-print(page_title, page_head)
+
+keys = all_products[0].keys()
+
+with open('products.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(all_products)
